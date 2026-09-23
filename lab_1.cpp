0abbf41b515ea21d0d3c08946aa8500431bf5cd2
Safina <safina.m.08@bk.ru>
2026-09-23 10:03:21 +0300

    add lab_1.cpp

---
+++ lab_1.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+int main(){
+    const int startingPoint{0};
+    const int HourMax{23};
+    const int MinuteMax{59};
+
+    const int startMorning{5};
+    const int noon{12};
+    const int startEvening{18};
+
+    const int one {1};
+    const int five{5};
+    const int ten {10};
+
+    int hour;
+    int minute;
+
+    std::cin >> hour >> minute;
+
+    if (hour > HourMax || hour < startingPoint || minute > MinuteMax || minute < startingPoint)
+    {
+        std::cout << "введены недопустимые данные";
+        return 0;
+    }
+
+    if (hour == startingPoint && minute == startingPoint)
+    {
+        std::cout << "полночь";
+        return 0;
+    }
+
+    if (hour == noon && minute == startingPoint)
+    {
+        std::cout << "полдень";
+        return 0;
+    }
+
+    int clock = hour;
+
+    if (hour > noon)
+    {
+        clock = hour - noon
+    }
+
+    std::cout << clock;
+
+    if (clock == one)
+    {
+        std::cout << "час";
+    }
+    else if (clock > one && clock < five)
+    {
+        std::cout << "часа";
+    }
+    else
+    {
+        std::cout << "часов";
+    }
+
+    if (minute != startingPoint)
+    {
+        std::cout << " " << minute;
+
+        if (minute == one)
+        {
+            std::cout << "минута"
+        }
+        else  if (minute > one && minute < five )
+        {
+            std::cout<< "минуты"
+        }
+        else
+        {
+            std::cout<< "минут";
+        }
+    }
+
+    if (hour >= startMorning && hour < noon)
+    {
+        std::cout<< "утра";
+    }
+    else if (hour > noon && hour << startEvening)
+    {
+        std::cout<< "дня";
+    }
+    else if (hour >= startEvening && hour <= HourMax)
+    {
+        std::cout<< "вечера";
+    }
+    else
+    {
+        std::cout<< "ночи";
+    }
+
+    return 0;
+
+
+
+
+
+}
